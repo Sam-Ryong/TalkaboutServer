9ebe8e884dc821b42dbeb3a5a5edd0922ec472a7
SamRyong <zackinthebox@khu.ac.kr>
2023-12-12 14:19:31 +0900

    세부 코드 수정 및 주석처리

--- main.js
+++ main.js
@@ -4,19 +4,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 //routers
-const chatrouter = require("./routers/webchatrout.js");
+//const router = require("./routers");
 
 // 변수 선언
 const app = express();
 
 // 미들웨어 설정과 정적 파일 서비스
 app.use(express.static(path.join(__dirname, "public")));
-app.use(redirectToHttps);
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
-//Route 정보
-app.use("/", chatrouter);
+//Router
+// app.use("/", chatrouter);
 
 app.listen(80, () => {
   console.log("Server is running on http://localhost:80");
